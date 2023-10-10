9fe2110be17b1247749719cdcff38a1820d9e8e6
Youssef Emad <96549295+YoussefEmad1222@users.noreply.github.com>
2023-10-10 23:24:37 +0300

    Add problem `Again Twenty Five!`

---
+++ Vjudge/CodeForces/630A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+    fio
+  long long int n;
+  cin>>n;
+   cout<<25;
+  
+   return 0;
+}
