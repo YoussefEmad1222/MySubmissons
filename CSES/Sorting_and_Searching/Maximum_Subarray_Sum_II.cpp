a804df7f79a1abc96fc7f1b75d6906d1882c22f4
Youssef Emad <96549295+YoussefEmad1222@users.noreply.github.com>
2023-10-10 23:17:45 +0300

    Add problem Maximum Subarray Sum II

---
+++ CSES/Sorting_and_Searching/Maximum_Subarray_Sum_II.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <iterator>
+
+// using namespace __gnu_pbds;
+using namespace std;
+// template <typename T>
+// using ordered_set =
+//     tree<T, null_type, less<T>, rb_tree_tag,
+//     tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<ll> vll;
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+const ll N = 2e5 + 5, INF = LONG_MAX, mod = 1e9 + 7;
+
+void fast() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+}
+string fromDecimalToBase(ll n, ll b) {
+  string ans = "";
+  while (n > 0) {
+    ans += to_string(n % b);
+    n /= b;
+  }
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
+// int largest_subarray(int a[], int n) {
+//   // Stores index of array elements
+//   unordered_map<int, int> index;
+//   int ans = 0;
+//   for (int i = 0, j = 0; i < n; i++) {
+
+//     // Update j based on previous
+//     // occurrence of a[i]
+//     j = max(index[a[i]], j);
+
+//     // Update ans to store maximum
+//     // length of subarray
+//     ans = max(ans, i - j + 1);
+
+//     // Store the index of current
+//     // occurrence of a[i]
+//     index[a[i]] = i + 1;
+//   }
+
+//   // Return final ans
+//   return ans;
+// }
+// recursive DFS
+// void dfs(int node, int parent, vector<vector<int>> &graph, vector<ll> &cnt) {
+//   bool flag = true;
+//   for (auto &child : graph[node]) {
+//     if (child != parent) {
+//       dfs(child, node, graph, cnt);
+//       cnt[node] += cnt[child];
+//       flag = false;
+//     }
+//   }
+//   if (flag) {
+//     cnt[node] = 1;
+//   }
+// }
+bool compare(pair<ll, ll> &a, pair<ll, ll> &b) { return a.second < b.second; }
+void solve() { // Maximum Sum SubArray II
+  int n, a, b;
+  cin >> n >> a >> b;
+  vector<int> vec(n + 1, 0);
+  for (int i = 0; i < n; i++) {
+    cin >> vec[i];
+  }
+  vector<ll> pref(n + 1, 0);
+  for (int i = 0; i < n; i++) {
+    pref[i + 1] = pref[i] + vec[i];
+  }
+  set<pair<ll, ll>> s;
+  for (int i = a; i <= b; i++) {
+    s.insert({pref[i], i});
+  }
+  ll maxx = -INF;
+  for (int i = 1; i <= n - a + 1; i++) {
+    maxx = max(maxx, s.rbegin()->first - pref[i - 1]);
+    s.erase({pref[i + a - 1], i + a - 1});
+    if (b + i <= n) {
+      s.insert({pref[b + i], b + i});
+    }
+  }
+  cout << maxx << endl;
+}
+
+int main() {
+  fast();
+  int t = 1;
+  // cin >> t;
+
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
