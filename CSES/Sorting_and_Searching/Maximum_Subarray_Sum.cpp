1ae08faf09201c9ffb19cc48c0fc198beb34b796
Youssef Emad <96549295+YoussefEmad1222@users.noreply.github.com>
2023-10-10 23:17:15 +0300

    Add problem Maximum Subarray Sum

---
+++ CSES/Sorting_and_Searching/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 7;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, ll> mcl;
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+#define all(x) (x).begin(), (x).end()
+#define oo 2e9 + 7
+const char nl = '\n';
+#define fast_cin()                                                             \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+
+void solve() { // missing number from 1 to n
+  int n;
+  cin >> n;
+  ll a[n];
+  vector<int> pref(n + 1, 0);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  ll ans = a[0], mx = a[0];
+  for (int i = 1; i < n; i++) {
+    ans = max(ans + a[i], a[i]);
+    mx = max(mx, ans);
+  }
+  cout << mx;
+}
+
+int main() {
+  fast_cin();
+  int t = 1;
+  while (t--)
+    solve();
+
+  return 0;
+}
